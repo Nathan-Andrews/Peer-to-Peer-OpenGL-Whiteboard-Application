ef5b3ba28026bd35908e4a0885d2ba07abaf5b35
Nathan-Andrews <andrewsnathan2003@gmail.com>
2024-11-16 15:15:36 -0600

    create project

---
+++ Peer-to-Peer-OpenGL-Whiteboard-Application/Main.cpp
@@ -0,0 +1,5 @@
+#include "Source.h"
+
+int main() {
+    std::cout << "Hello World!\n";
+}
---
+++ Peer-to-Peer-OpenGL-Whiteboard-Application/Source.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// standard includess
+#include <iostream>
+
+// library includes
+
+// local includes
