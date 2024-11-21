81913cc62c16fd6a47098a4c9b70104457c1743d
Nathan-Andrews <andrewsnathan2003@gmail.com>
2024-11-20 20:33:59 -0600

    add source for conncurrent queue

--- Peer-to-Peer-OpenGL-Whiteboard-Application/Source.h
+++ Peer-to-Peer-OpenGL-Whiteboard-Application/Source.h
@@ -6,11 +6,17 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <optional>
 
 // library includes
 #include <boost/asio.hpp>
 #include <GLFW/glfw3.h>
 
 // local includes
+#include "ConcurrentQueue.h"
 #include "Connection.h"
 #include "Server.h"
+#include "ConnectionManager.h"
