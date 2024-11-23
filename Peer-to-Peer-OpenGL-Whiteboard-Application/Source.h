8edb3bd59a70b70954d2bae6bc35b6d1e59a174e
Nathan-Andrews <andrewsnathan2003@gmail.com>
2024-11-22 18:27:39 -0600

    added new typedefs

--- Peer-to-Peer-OpenGL-Whiteboard-Application/Source.h
+++ Peer-to-Peer-OpenGL-Whiteboard-Application/Source.h
@@ -15,12 +15,16 @@
 #include <boost/asio.hpp>
 #include <GLFW/glfw3.h>
 
+// typedefs
+typedef u_short PORT;
+typedef std::string HOST;
+
+// macro constants
+#define LOCALHOST "127.0.0.1"
+#define MAX_BUFFER_SIZE 1024
+
 // local includes
 #include "ConcurrentContainer.h"
 #include "Connection.h"
 #include "Server.h"
 #include "ConnectionManager.h"
-
-
-#define LOCALHOST "127.0.0.1"
-#define MAX_BUFFER_SIZE 1024
