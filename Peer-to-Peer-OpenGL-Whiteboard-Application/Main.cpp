f84b2f4610c656092e81c35496362dca61a26ad3
Nathan-Andrews <andrewsnathan2003@gmail.com>
2024-11-23 00:35:49 -0600

    remove test-code from main

--- Peer-to-Peer-OpenGL-Whiteboard-Application/Main.cpp
+++ Peer-to-Peer-OpenGL-Whiteboard-Application/Main.cpp
@@ -1,42 +1,6 @@
 #include "Source.h"
 
-int inputThread(ConnectionManager& manager) {
-    std::string message;
-
-    while (true) {
-        std::getline(std::cin, message);
-        
-        manager.Write(message);
-    }
-}
-
 int main() {
-    try {
-        std::cout << "Enter mode (1 = server, 2 = client): ";
-        int mode;
-        std::cin >> mode;
-        std::cin.ignore(); // Clear the newline character
-
-
-        if (mode == 1) {
-            // Server mode
-            Server();
-        } else if (mode == 2) {
-            // Client mode
-            ConnectionManager manager(LOCALHOST,11111);
-
-            std::thread(inputThread,std::ref(manager)).detach();
-            while (true) {
-                std::string message = manager.Read();
-                std::cout << "recieved: " << message << std::endl;
-            }
-
-        } else {
-            std::cerr << "Invalid mode. Exiting." << std::endl;
-        }
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
 
     return 0;
 }
