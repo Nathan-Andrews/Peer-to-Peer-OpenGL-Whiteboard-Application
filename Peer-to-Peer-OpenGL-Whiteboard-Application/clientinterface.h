0e87bb2e42e8ef68c73498dd767428442ed60bc0
Raghav Sood <142909084+soodraghav15@users.noreply.github.com>
2024-11-29 02:17:46 -0600

    Add files via upload
    Initial commit

---
+++ Peer-to-Peer-OpenGL-Whiteboard-Application/clientinterface.h
@@ -0,0 +1,28 @@
+#ifndef CLIENTINTERFACE_H
+#define CLIENTINTERFACE_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class ClientInterface;
+}
+QT_END_NAMESPACE
+
+class ClientInterface : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ClientInterface(QWidget *parent = nullptr);
+    ~ClientInterface();
+
+private slots:
+    void on_btnjoin_clicked();
+
+    void on_modeChanged(int index);
+
+private:
+    Ui::ClientInterface *ui;
+};
+#endif // CLIENTINTERFACE_H
