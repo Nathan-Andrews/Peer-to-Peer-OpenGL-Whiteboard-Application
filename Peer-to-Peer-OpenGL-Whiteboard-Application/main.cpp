0517276806adb39b75819e11e9e3d6d5972ee0ba
Nathan-Andrews <andrewsnathan2003@gmail.com>
2024-11-30 16:52:21 -0600

    update main

--- Peer-to-Peer-OpenGL-Whiteboard-Application/main.cpp
+++ Peer-to-Peer-OpenGL-Whiteboard-Application/main.cpp
@@ -1,6 +1,24 @@
+//  Include the header file for the ClientInterface class, main GUI for application
 #include "Source.h"
 
-int main() {
+// // Include QApplication to handle application-wide settings and resources.
 
-    return 0;
+// #include "Source.h"
+
+int main(int argc, char *argv[])
+{
+    // Create a QApplication object and It is needed for managing GUI resources and control flow.
+    QApplication a(argc, argv);
+
+    // Create an instance of the ClientInterface class (the main window).
+    ClientInterface w;
+
+    // Display the main window by calling the 'show()' method.
+    w.show();
+
+    // The call to 'a.exec()' starts the Qt application event loop.
+    return a.exec(); // The return value is used to exit the program cleanly
+
+    // std::cout << "hello world\n";
+    // return 0;
 }
