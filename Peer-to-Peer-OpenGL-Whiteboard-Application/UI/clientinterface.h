fe30774a6b7d1491ebb73a70f6067442dbb57b42
Nathan-Andrews <andrewsnathan2003@gmail.com>
2024-12-01 12:42:33 -0600

    added comments and new types for the FormDataType enum

--- Peer-to-Peer-OpenGL-Whiteboard-Application/UI/clientinterface.h
+++ Peer-to-Peer-OpenGL-Whiteboard-Application/UI/clientinterface.h
@@ -4,9 +4,11 @@
 #include "Source.h"
 
 enum FormDataType {
-    HOST_SERVER,
-    JOIN_SERVER,
-    SESSION_LINK,
+    HOST_SERVER, // indicates that the user pressed the "create session" button
+    JOIN_SERVER, // indicates that the user pressed the "join server" button
+    SESSION_LINK, // indicates that the server is sending the ip and port do be displayed in the window as a session ID
+    JOIN_SUCCESS, // indicates that the code was valid.  Which means the GUI should close
+    JOIN_FAIL, // indicates that the join failed (invalid code).  So the user needs to input a new code
     NONE
 };
 
